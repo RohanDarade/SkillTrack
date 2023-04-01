c5184fcd51b4e6077abacb07900bb29195e7adb2
Rohan Darade <rohandarade29@gmail.com>
2023-04-02 02:02:54 +0530

    Firebase Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import {Routes , Route}from "react-router"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/" element={<Signup/>}/>
+      </Routes>
     </div>
   );
 }
