66dd2100e852e574ad5089272845fe05cadcdfe8
Rohan Darade <rohandarade29@gmail.com>
2023-04-02 16:48:35 +0530

    auth context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import {Routes , Route}from "react-router"
 import './App.css';
-import LandingPage from "./LandingPage";
-import Signup from "./Signup";
-import MuiTest from "./muiTest";
-
+import LandingPage from "./components/LandingPage";
+import Signup from "./components/Signup";
+import MuiTest from "./components/muiTest";
+import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/Signup" element={<Signup/>}/>
+        <Route path="/Login" element={<Login/>}/>
         <Route path="/muiTest" element={<MuiTest/>}/>
       </Routes>
     </div>
