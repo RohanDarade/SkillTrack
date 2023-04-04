74ce0fe98870c51988f69b8b8f46af9342c43c33
Rohan Darade <rohandarade29@gmail.com>
2023-04-04 12:51:33 +0530

    done with authentication'

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,47 @@
-import {Routes , Route}from "react-router"
-import './App.css';
-import LandingPage from "./components/LandingPage";
+import React from "react";
 import Signup from "./components/Signup";
-import MuiTest from "./components/muiTest";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./context/AuthContext";
+import { Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./components/ForgotPassword";
+import UpdateProfile from "./components/UpdateProfile";
+
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-        <Route path="/Signup" element={<Signup/>}/>
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/muiTest" element={<MuiTest/>}/>
-      </Routes>
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <AuthProvider>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            ></Route>
+            <Route
+              path="/update-profile"
+              element={
+                <PrivateRoute>
+                  <UpdateProfile />
+                </PrivateRoute>
+              }
+            ></Route>
+
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgotPassword" element={<ForgotPassword />} />
+          </Routes>
+        </AuthProvider>
+      </div>
+    </Container>
   );
 }
 
